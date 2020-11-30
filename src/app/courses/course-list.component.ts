fbaa755fdf9b5e945f8510f59688252db71b230a
Thiago Aquino <thiagoaquiino@gmail.com>
2020-11-30 11:34:18 -0300

    Adicionando o delete

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -31,9 +31,17 @@ export class CourseListComponent implements OnInit {
                 this.filteredCourses = this._courses;
             },
             error: err => console.log('Error', err)
-        })
-       
+        })   
+    }
 
+    deleteById(courseId:number) : void {
+        this.courseService.deleteById(courseId).subscribe({
+            next: () => {
+                console.log('Deleted with success');
+                this.retrieveAll();
+            },
+            error: err => console.log('Error', err)
+        })
     }
 
     set filter(value: string){
