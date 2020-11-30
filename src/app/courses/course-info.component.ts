2a3576ea4d01a77852ef98dfa7f99b117e2dbd39
Thiago Aquino <thiagoaquiino@gmail.com>
2020-11-30 10:04:46 -0300

    Adicionando formularios e templates

--- src/app/courses/course-info.component.ts
+++ src/app/courses/course-info.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Course } from './course';
+import { CourseService } from './course.service';
 
 @Component ({
     templateUrl: './course-info.component.html'
@@ -7,12 +9,15 @@ import { ActivatedRoute } from '@angular/router';
 
 export class CourseInfoComponent  implements OnInit {
    
-    courseId: number;
+    course: Course;
     
-    constructor(private activatedRoute: ActivatedRoute){}
+    constructor(private activatedRoute: ActivatedRoute, private coursService: CourseService){}
     ngOnInit():  void {
-        this.courseId = +this.activatedRoute.snapshot.paramMap.get('id');
+        this.course = this.coursService.retrieveById(Number(this.activatedRoute.snapshot.paramMap.get('id')));
 
     }
 
+    save(): void {
+        this.coursService.save(this.course);
+    }
 }
