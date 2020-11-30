bcb19b174494d2e9a5789c9fadeb05008c881536
Thiago Aquino <thiagoaquiino@gmail.com>
2020-11-30 12:41:23 -0300

    Tratando erros

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './core/component/nav-bar/nav-bar.component';
-import { Error404Component } from './erro-404/error-404.component';
 import { CourseModule } from './courses/course.module';
 import { CoreModule } from './core/core.module';
 
@@ -14,8 +11,6 @@ import { CoreModule } from './core/core.module';
 @NgModule({
   declarations: [
     AppComponent,
-
-    Error404Component
     
   ],
   imports: [
@@ -26,9 +21,6 @@ import { CoreModule } from './core/core.module';
     RouterModule.forRoot([
       {
         path: '', redirectTo: 'courses', pathMatch: 'full'
-      },
-      {
-        path: '**', component: Error404Component
       }
     ])
 
