5ea39fdb3985db6827b513180e570905964df721
Thiago Aquino <thiagoaquiino@gmail.com>
2020-11-30 12:04:52 -0300

    segregando a aplicação em módulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,37 +5,23 @@ import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
-import { CourseListComponent } from './courses/course-list.component';
-import { CourseInfoComponent } from './courses/course-info.component';
-import { StarComponent } from './star/star.component';
-import { ReplacePipe } from './pipe/replace.pipe';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { Error404Component } from './erro-404/error-404.component';
+import { CourseModule } from './courses/course.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseListComponent,
-    StarComponent,
-    ReplacePipe,
     NavBarComponent,
-    Error404Component,
-    CourseInfoComponent
-
+    Error404Component
     
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
+    CourseModule,
     RouterModule.forRoot([
-      {
-        path: 'courses', component: CourseListComponent
-      },
-      {
-        path: 'courses/info/:id', component: CourseInfoComponent
-      },
       {
         path: '', redirectTo: 'courses', pathMatch: 'full'
       },
